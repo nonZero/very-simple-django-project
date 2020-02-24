6c576a94c4a1cde30b99b2e3f212ae97bd19470a
Udi Oron <udioron@gmail.com>
2020-02-24 11:26:28 +0200

    preparing for heroku

--- mydemo/settings.py
+++ mydemo/settings.py
@@ -1,5 +1,7 @@
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -102,3 +104,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
